6cb9eac191ec481ebc21bb63be680d13f4d2b241
diegomarcon1 <diegomarcon1@hotmail.com>
2022-06-13 21:59:38 -0300

    Updated to apply the private set funcion of CPF property

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,10 +3,13 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Diego"
     var cpf: String = "123.456.789-12"
+    private set //Sera privado somente o set e não o get dentro desta funcao Pessoa
 
 }
 fun main(){
     val Diego = Pessoa()
     println(Diego.nome)
     println(Diego.cpf)
+
+
 }
