009cb0f15496aaf303e662489453c1c749e44936
diegomarcon1 <diegomarcon1@hotmail.com>
2022-06-20 21:49:09 -0300

    Created Double array of test class

---
+++ Application/api-collections/src/main/kotlin/one/digitaninnovation/collections/TesteArrayString.kt
@@ -0,0 +1,23 @@
+package one.digitaninnovation.collections
+
+fun main(){
+    val nomes = Array(4){""}
+    nomes[0] = "Maria"
+    nomes[1] = "Patricia"
+    nomes[2] = "Karol"
+    nomes[3] = "Gaby"
+    for(nome in nomes){
+        println(nome)
+    }
+    println("------------------")
+    nomes.sort()
+    nomes.forEach {
+        println(it) }
+
+    println("------------------")
+    val nomes2 = arrayOf("Ana", "Maria", "Patricia")
+    nomes2.sort()
+    nomes2.forEach {
+        println(it)
+    }
+}
---
+++ Application/api-collections/src/main/kotlin/one/digitaninnovation/collections/TesteDoubleArray.kt
@@ -0,0 +1,28 @@
+package one.digitaninnovation.collections
+
+import kotlin.math.sqrt
+
+fun main(){
+    val salarios = DoubleArray(3)
+    salarios[0] = 1000.0
+    salarios[1] = 2850.0
+    salarios[2] = 589.0
+
+    salarios.forEach {
+        println(it)
+    }
+    println("-----------------")
+    salarios.forEachIndexed { index, salario ->
+        salarios[index] = salario * 1.1
+    }
+    println("-----------------")
+    salarios.forEach {
+        println(it) }
+
+    println("-----------------")
+    val salarios2 = doubleArrayOf(1000.0, 2000.0, 3000.0)
+    salarios2.sort()
+    salarios2.forEach {
+        println(it*1.2)
+    }
+}
