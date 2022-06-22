86263a756fd2d80883650836c31b865474a4fed6
diegomarcon1 <diegomarcon1@hotmail.com>
2022-06-21 22:02:09 -0300

    Created listof of test class Added example of the sortedBy

---
+++ Application/api-collections/src/main/kotlin/one/digitaninnovation/collections/TesteList.kt
@@ -0,0 +1,30 @@
+package one.digitaninnovation.collections
+
+fun main(){
+    val joao = Funcionario("Joao", 3000.0)
+    val pedro = Funcionario("Pedro", 2000.0)
+    val maria = Funcionario("Maria", 4000.0)
+
+    val funcionarios = listOf(joao, pedro, maria)
+    funcionarios.forEach{println(it)}
+
+    println("---------------")
+    println(funcionarios.find{it.nome == "Maria"})
+
+    println("---------------")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach{ println(it)}
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double
+) {
+
+    override fun toString(): String =
+        """
+        Nome: $nome
+        Salario: $salario
+    """.trimIndent()
+}
