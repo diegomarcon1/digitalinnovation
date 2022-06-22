28d7c754d8e248338df28b8b9609783ea801969b
diegomarcon1 <diegomarcon1@hotmail.com>
2022-06-21 22:14:28 -0300

    Created listof of test class Added example of the sortedBy, groupBy

--- Application/api-collections/src/main/kotlin/one/digitaninnovation/collections/TesteList.kt
+++ Application/api-collections/src/main/kotlin/one/digitaninnovation/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitaninnovation.collections
 
 fun main(){
-    val joao = Funcionario("Joao", 3000.0)
-    val pedro = Funcionario("Pedro", 2000.0)
-    val maria = Funcionario("Maria", 4000.0)
+    val joao = Funcionario("Joao", 3000.0, "CLT")
+    val pedro = Funcionario("Pedro", 2000.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
 
     val funcionarios = listOf(joao, pedro, maria)
     funcionarios.forEach{println(it)}
@@ -15,11 +15,18 @@ fun main(){
     funcionarios
         .sortedBy { it.salario }
         .forEach{ println(it)}
+
+    println("---------------")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach{ println(it)}
+
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ) {
 
     override fun toString(): String =
